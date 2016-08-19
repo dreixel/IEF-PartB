427825b13bd7a1d790468610cfdde04b3136ca44
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2016-08-19 20:22:05 +0200

    to get references correctly displayed in separate documents 1 and 2

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ pdflatex IF-2016-Part_B.tex
 pdflatex -jobname=document1 "\includeonly{doc1}\input{IF-2016-Part_B}"
 pdflatex -jobname=document2 "\includeonly{doc2}\input{IF-2016-Part_B}"
 #bibtex IF-2016-Part_B
-pdflatex IF-2016-Part_B.tex
+#bibtex document1
+#bibtex document2
 pdflatex IF-2016-Part_B.tex
 pdflatex -jobname=document1 "\includeonly{doc1}\input{IF-2016-Part_B}"
 pdflatex -jobname=document2 "\includeonly{doc2}\input{IF-2016-Part_B}"
